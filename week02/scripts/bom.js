a0ee978d517aa8c083dd3671413b93c59437c472
manntrr <tmann@byupathway.edu>
2025-09-11 11:39:45 -0400

    DOM events section completed.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,64 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
+const registry = {};
+let count = 0;
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-deleteButton.ariaLabel = 'Remove '+input.value;
-li.append(deleteButton);
-list.append(li);
+button.addEventListener("click",clickAdd);
+function clickAdd(event) {
+    if(input.value.trim() !== '') {
+        const id = input.value.trim();
+        if(!(id in registry)) {
+            const li = document.createElement('li');
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = '❌';
+            deleteButton.ariaLabel = 'Remove '+id;
+            deleteButton.addEventListener("click",clickDelete);
+            deleteButton.id = id;
+            li.textContent = id;
+            li.append(deleteButton);
+            list.append(li);
+            count++;
+            registry[id] = li;
+            input.value='';
+        } else {
+            console.log("add button clicked with an already existing input value.  moving focus to the input field.")
+        }
+    }
+    else {
+        console.log("add button clicked with empty input value.  moving focus to the input field.")
+    }
+    if(count >= 10) {
+        input.disabled = true;
+        input.style.visibility = 'hidden';
+        button.disabled = true;
+        button.style.visibility = 'hidden';
+    }
+    else{
+        input.disabled = false;
+        input.style.visibility = 'visible';
+        button.disabled = false;
+        button.style.visibility = 'visible';
+    }
+    input.focus();
+}
+function clickDelete(event) {
+    const id = event.target.id;
+    const li = registry[id];
+    if(li !== '') list.removeChild(li);
+    delete registry[id];
+    count--;
+     if(count >= 10) {
+        input.disabled = true;
+        input.style.visibility = 'hidden';
+        button.disabled = true;
+        button.style.visibility = 'hidden';
+    }
+    else{
+        input.disabled = false;
+        input.style.visibility = 'visible';
+        button.disabled = false;
+        button.style.visibility = 'visible';
+    }
+   input.focus();
+}
